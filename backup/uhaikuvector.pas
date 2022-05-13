39ebdfcca22e9e87db8777691a856e58cd99dcbb
Rastrelly <rastrelly@gmail.com>
2022-05-13 16:33:21 +0300

    added bg image fetching

--- backup/uhaikuvector.pas
+++ backup/uhaikuvector.pas
@@ -10,6 +10,8 @@ uses
 
 type
 
+  sa = array of string;
+
   { TForm1 }
 
   TForm1 = class(TForm)
@@ -30,6 +32,7 @@ type
     procedure Edit3Change(Sender: TObject);
     procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
     procedure FormCreate(Sender: TObject);
+    procedure FormShow(Sender: TObject);
     procedure Image1Click(Sender: TObject);
   private
 
@@ -67,7 +70,7 @@ var
                                 PDV : PDesignVector): Int64; StdCall;
                                 External 'GDI32.dll' Name 'AddFontResourceExA';
 
- Function RemoveFontResourceEx (Dir : PAnsiChar;
+  Function RemoveFontResourceEx (Dir : PAnsiChar;
                                 Flag: Cardinal;
                                 PDV : PDesignVector): Int64; StdCall;
                                 External 'GDI32.dll' Name 'RemoveFontResourceExA';
@@ -83,19 +86,48 @@ var t,fn:string;
 begin
   fn:=extractfiledir(application.exename)+'\';
   assignfile(f,fn+'config.txt');
+
   reset(f);
+
   readln(f,fntf);
   readln(f,fntn);
+
   CloseFile(f);
 end;
 
+
+function loadtoarray(fn,fa:string):sa;
+var t:string;
+    arr:array of string;
+begin
+  assignfile(f,fn+fa);
+  setlength(arr,0);
+  reset(f);
+  while not eof(f) do
+  begin
+    readln(f,t);
+    setlength(arr,length(arr)+1);
+    arr[high(arr)]:=t;
+  end;
+  closefile(f);
+
+  result:=arr;
+end;
 
 procedure loaddata;
 var t,fn:string;
 begin
   fn:=extractfiledir(application.exename)+'\vocabulary\';
 
-  assignfile(f,fn+'templates.txt');
+  tmp:=loadtoarray(fn,'templates.txt');
+  n:=loadtoarray(fn,'n.txt');
+  v:=loadtoarray(fn,'v.txt');
+  p:=loadtoarray(fn,'p.txt');
+  a:=loadtoarray(fn,'a.txt');
+  s:=loadtoarray(fn,'s.txt');
+  m:=loadtoarray(fn,'m.txt');
+
+  {assignfile(f,fn+'templates.txt');
   reset(f);
   while not eof(f) do begin readln(f,t); setlength(tmp,length(tmp)+1); tmp[high(tmp)]:=t; end;
   closefile(f);
@@ -128,7 +160,7 @@ begin
   assignfile(f,fn+'s.txt');
   reset(f);
   while not eof(f) do begin readln(f,t); setlength(s,length(s)+1); s[high(s)]:=t; end;
-  closefile(f);
+  closefile(f);  }
 
 end;
 
@@ -202,15 +234,17 @@ end;
 
 procedure TForm1.FormCreate(Sender: TObject);
 Var
+   i:integer;
    strAppPath: String;
+   fffff:array of integer;
 begin
   //font loading from: https://forum.lazarus.freepascal.org/index.php/topic,21032.0.html
+
+  loadconfig;
 
   randomize;
   lw:=UTF8Length (vowels);
   loaddata;
-
-
 
   strAppPath:= ExtractFileDir(Application.ExeName);
   If FileExists(strAppPath+'\'+fntf) Then
@@ -234,15 +268,35 @@ begin
       ShowMessage('Font add failed.');
     end;
   end
-  else ShowMessage('File not found: '+strAppPath+'japanesebrushrusbyme.otf');
+  else ShowMessage('File not found: '+strAppPath+'\'+fntf);
 
+end;
+
+procedure TForm1.FormShow(Sender: TObject);
+var Image:TJPEGImage;
+    bmpi:Graphics.TBitmap;
+    mr1,mr2:TRect;
+begin
+  Image := TJPEGImage.Create;
+  Image.LoadFromFile(ExtractFileDir(Application.ExeName)+'\bg.jpg');
+  bmpi:=Graphics.TBitmap.Create;
+  bmpi.Width:=image.Width;
+  bmpi.Height:=image.Height;
+  bmpi.Assign(image);
+  Image1.Picture.Bitmap.Width:=bmpi.Width;
+  Image1.Picture.Bitmap.Height:=bmpi.Height;
+  mr1.Create(0,0,bmpi.Width,bmpi.Height);
+  mr2.Create(0,0,image1.Width,image1.Height);
+  Image1.Canvas.CopyRect(mr2,bmpi.Canvas,mr1);
+  Image.Destroy;
+  bmpi.Destroy;
 end;
 
 procedure TForm1.Edit1Change(Sender: TObject);
 var cnv:integer;
 begin
   cnv:=calcvowels(edit1.text);
-  if (cnv=0) or (cnv>7) then Label1.Font.Color:=clRed else Label1.Font.Color:=clGreen;
+  if (cnv<3) or (cnv>7) then Label1.Font.Color:=clRed else Label1.Font.Color:=clGreen;
   Label1.Caption:=inttostr(cnv);;
 end;
 
@@ -260,7 +314,7 @@ procedure TForm1.Edit2Change(Sender: TObject);
 var cnv:integer;
 begin
   cnv:=calcvowels(edit2.text);
-  if (cnv=0) or (cnv>10) then Label2.Font.Color:=clRed else Label2.Font.Color:=clGreen;
+  if (cnv<5) or (cnv>10) then Label2.Font.Color:=clRed else Label2.Font.Color:=clGreen;
   Label2.Caption:=inttostr(cnv);
 end;
 
@@ -268,7 +322,7 @@ procedure TForm1.Edit3Change(Sender: TObject);
 var cnv:integer;
 begin
   cnv:=calcvowels(edit3.text);
-  if (cnv=0) or (cnv>7) then Label3.Font.Color:=clRed else Label3.Font.Color:=clGreen;
+  if (cnv<3) or (cnv>7) then Label3.Font.Color:=clRed else Label3.Font.Color:=clGreen;
   Label3.Caption:=inttostr(cnv);
 end;
 
@@ -276,11 +330,11 @@ procedure TForm1.FormClose(Sender: TObject; var CloseAction: TCloseAction);
 Var
   AppPath: String;
 Begin
- AppPath:= ExtractFileDir(Application.ExeName);
+  AppPath:= ExtractFileDir(Application.ExeName);
 
-  If FileExists(AppPath+'\japanesebrushrusbyme.otf')
+  If FileExists(AppPath+'\'+fntf)
   Then
-   If RemoveFontResourceEx(PAnsiChar(AppPath+'\japanesebrushrusbyme.otf'), FR_Private, Nil) <> 0
+   If RemoveFontResourceEx(PAnsiChar(AppPath+'\'+fntf), FR_Private, Nil) <> 0
    Then SendMessage(Form1.Handle, WM_FONTCHANGE, 0, 0);
 End;
 
